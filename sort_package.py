5fbd9b49e89efbc6f31af8e4073fa8e85bbf76c6
kenshiro <s.kennshiroussi@gmail.com>
2022-08-11 15:27:53 +0900

    '#'

--- sort_package.py
+++ sort_package.py
@@ -1,7 +1,6 @@
 with open("requirements.txt", "r") as f:
     package_list = []
     package_list = sorted(f.readlines())
-    print(package_list)
 
 with open("requirements.txt", "w") as f:
     f.writelines(package_list)
